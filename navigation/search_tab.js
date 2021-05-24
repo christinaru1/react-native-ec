b66dacfa65372bd946791c64b80b59489798bade
christinaru1 <christina.ru.21@dartmouth.edu>
2021-05-24 15:14:45 -0400

    first commit

--- navigation/search_tab.js
+++ navigation/search_tab.js
@@ -1,30 +1,48 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-// import { Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import Upload from '../components/upload';
+import UploadHeader from '../components/header';
 import VideoList from '../components/video_list';
 import VideoDetail from '../components/video_detail';
-
-// const TempDetail = (props) => (<Button onPress={() => { props.navigation.pop(); }} title="close" />);
 
 const Stack = createStackNavigator();
 
 // nest stack navigator to handle two internal views
 // "name" prop is the name of the route
-
-const SearchTab = () => {
+const SearchUploadNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Search"
+    <Stack.Navigator mode="modal" initialRouteName="SearchScreen">
+      <Stack.Screen name="SearchScreen"
         component={VideoList}
         options={{
-          title: 'Youtube Search',
           headerStyle: {
             backgroundColor: '#f4511e',
           },
           headerTintColor: '#fff',
+          headerTitle: (props) => <UploadHeader {...props} title="YouTube Search" />,
         }}
+      />
+      <Stack.Screen name="Upload"
+        component={Upload}
+        options={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitle: (props) => <UploadHeader {...props} title="Upload a Video" />,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const SearchTab = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Search"
+        component={SearchUploadNavigator}
+        options={{ headerShown: false }}
       />
       <Stack.Screen name="Detail" component={VideoDetail} />
     </Stack.Navigator>
